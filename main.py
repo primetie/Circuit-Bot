b1d9378d7738cc330db3ab396dcd370a661e378d
primetie <oliverbelsey@icloud.com>
2024-11-12 19:35:40 +0000

    edit to main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import discord
 import os
+token = os.environ['token']
 
 class Client(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
         
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = Client(intents=intents)
-client.run(os.getenv("token"))
+client.run(token)
 
