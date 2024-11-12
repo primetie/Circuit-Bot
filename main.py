09e9e76b2d340475ad7dd9df72a0ccaa44f032b7
primetie <oliverbelsey@icloud.com>
2024-11-12 19:05:59 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 
 class Client(dicord.Client):
     async def on_ready(self):
-        print('Logged on as {self.user}!')+
+        print('Logged on as {self.user}!')
         
 intents = discord.Intents.default()
 intents.message_content = True
