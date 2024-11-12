b270ed7cb5da2a74c4c6dd14c8443563af05dfe0
primetie <oliverbelsey@icloud.com>
2024-11-12 20:21:33 +0000

    logging in main.py

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ token = os.environ['token']
 class Client(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
+        
     async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+        print(message.channel)
+        if message.author == self.user:
+            return
+        logs = client.get_channel(1305980871127601244)
+        await logs.send(f'{message.author} said {message.content}')
         
 intents = discord.Intents.default()
 intents.message_content = True
